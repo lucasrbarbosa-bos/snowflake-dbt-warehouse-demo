73d330ca093f921e4b80ee28c53e7335b7ec5417
Lucas Barbosa <lucas.barbosa@bosolutionsai.com>
2025-10-05 22:54:14 -0300

    trying proposed changes

--- deploy_parametrized.sql
+++ deploy_parametrized.sql
@@ -1,7 +1,10 @@
 --!jinja
--- Forwards variables to both repo-hosted scripts.
+-- This file MUST reference repo files via the repo path, not relative paths.
+-- Required -D variables (from the workflow): repo_name, branch,
+-- env, db_name, dbt_project_object, wh, src_db, src_schema, daily_cron
+-- Optional: extra_args
 
-EXECUTE IMMEDIATE FROM 'deploy/streams.sql'
+EXECUTE IMMEDIATE FROM @{{ repo_name }}/branches/{{ branch }}/deploy/streams.sql
   USING (
     env                => '{{ env }}',
     db_name            => '{{ db_name }}',
@@ -12,7 +15,7 @@ EXECUTE IMMEDIATE FROM 'deploy/streams.sql'
     extra_args         => '{{ extra_args | default("") }}'
   );
 
-EXECUTE IMMEDIATE FROM 'deploy/schedules.sql'
+EXECUTE IMMEDIATE FROM @{{ repo_name }}/branches/{{ branch }}/deploy/schedules.sql
   USING (
     env                => '{{ env }}',
     db_name            => '{{ db_name }}',
