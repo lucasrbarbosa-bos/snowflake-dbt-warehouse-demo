d6edcd8785b8f8f09b582ab859d8dd32aa76754c
Lucas Barbosa <lucas.barbosa@bosolutionsai.com>
2025-10-05 22:34:48 -0300

    reversing changes - updating deploy parametrized

--- dbt_deploy/deploy_parametrized.sql
+++ dbt_deploy/deploy_parametrized.sql
@@ -1,24 +1,18 @@
 --!jinja
--- Forward env/dbt parameters into the two deploy scripts stored in the Git repository clone.
--- REQUIRED USING vars:
---   env, db_name, dbt_project_object, wh, src_db, src_schema, daily_cron
--- OPTIONAL:
---   extra_args  (default: '')
+-- Forwards variables to both repo-hosted scripts.
 
--- Evented CUSTOMERS flow
-EXECUTE IMMEDIATE FROM @snowflake_dbt_repo/branches/main/deploy/streams.sql
+EXECUTE IMMEDIATE FROM @{{ repo_name }}/branches/{{ branch }}/deploy/streams.sql
   USING (
-    env              => '{{ env }}',
-    db_name          => '{{ db_name }}',
+    env                => '{{ env }}',
+    db_name            => '{{ db_name }}',
     dbt_project_object => '{{ dbt_project_object }}',
-    wh               => '{{ wh }}',
-    src_db           => '{{ src_db }}',
-    src_schema       => '{{ src_schema }}',
-    extra_args       => '{{ extra_args | default("") }}'
+    wh                 => '{{ wh }}',
+    src_db             => '{{ src_db }}',
+    src_schema         => '{{ src_schema }}',
+    extra_args         => '{{ extra_args | default("") }}'
   );
 
--- Daily schedule
-EXECUTE IMMEDIATE FROM @snowflake_dbt_repo/branches/main/deploy/schedules.sql
+EXECUTE IMMEDIATE FROM @{{ repo_name }}/branches/{{ branch }}/deploy/schedules.sql
   USING (
     env                => '{{ env }}',
     db_name            => '{{ db_name }}',
